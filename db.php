2df63449877e03c6dd229ae97580cf9fecdaca62
Enric Rami <enricrami@gmail.com>
2025-04-14 15:20:19 +0200

    First Commit

---
+++ db.php
@@ -0,0 +1,20 @@
+
+<?php
+$host = 'localhost';
+$dbname = 'simple_crud';
+$username = 'root';
+$password = '123456780';
+
+$conn = new mysqli($host, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die(json_encode(["success" => false, "message" => "Database Connection Failed: " . $e->getMessage()]));
+}
+?>
